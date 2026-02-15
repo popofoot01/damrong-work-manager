37734d9d3f6033c5a63b016dd1c4a291c73d3437
Wichaya SmU <wichaya@gmail.com>
2026-02-16 01:14:15 +0700

    thai date format

--- server.js
+++ server.js
@@ -1574,12 +1574,61 @@ button{
 
 
 <script>
-
-
-
 addItem(); // เริ่มต้นมี 1 รายการ
 
 let items = [];
+
+
+function renderItems() {
+  const container = document.getElementById("itemsContainer");
+  container.innerHTML = "";
+
+  let grandTotal = 0;
+
+  items.forEach((item, index) => {
+
+    item.total = item.width * item.height * item.qty * item.rate;
+    grandTotal += item.total;
+
+    container.innerHTML +=
+  '<div style="background:#1e293b;padding:15px;margin:10px 0;border-radius:8px;">' +
+
+  'กว้าง:' +
+  '<input type="number" step="0.01" ' +
+  'value="' + item.width + '" ' +
+  'onchange="updateItem(' + index + ', \'width\', this.value)">' +
+
+  'สูง:' +
+  '<input type="number" step="0.01" ' +
+  'value="' + item.height + '" ' +
+  'onchange="updateItem(' + index + ', \'height\', this.value)">' +
+
+  'จำนวน:' +
+  '<input type="number" ' +
+  'value="' + item.qty + '" ' +
+  'onchange="updateItem(' + index + ', \'qty\', this.value)">' +
+
+  'ราคา/ตรม:' +
+  '<input type="number" ' +
+  'value="' + item.rate + '" ' +
+  'onchange="updateItem(' + index + ', \'rate\', this.value)">' +
+
+  '<br><br>' +
+
+  '<strong>รวม: ' + item.total.toFixed(2) + ' บาท</strong>' +
+
+  '<button type="button" ' +
+  'onclick="removeItem(' + index + ')" ' +
+  'style="background:red;color:white;border:none;padding:5px 10px;border-radius:5px;margin-left:10px;">ลบ</button>' +
+
+  '</div>';
+  });
+
+  document.getElementById("grandTotal").innerText = grandTotal.toFixed(2);
+  document.getElementById("finalPrice").value = grandTotal.toFixed(2);
+  document.getElementById("itemsInput").value = JSON.stringify(items);
+}
+
 
 function updateItem(index, field, value) {
   items[index][field] = parseFloat(value) || 0;
